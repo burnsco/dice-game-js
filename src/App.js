906faf12ce083bccab3b11d4732e9ee65133e2c2
Corey Burns <coreymburns@gmail.com>
2019-11-24 09:30:09 -0500

    centered dice

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   }
 
   return (
-    <>
+    <div className="App-header">
       <div className={rolling ? 'dice shakeDice' : 'dice'}>
         <Die />
         <Die />
@@ -29,7 +29,7 @@ const App = () => {
       >
         <p>Roll</p>
       </button>
-    </>
+    </div>
   )
 }
 
