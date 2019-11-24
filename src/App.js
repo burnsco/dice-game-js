500be6236c93163741ee4b3c0360dc63f33ab2c9
Corey Burns <coreymburns@gmail.com>
2019-11-24 10:06:52 -0500

    refactored, finished

--- src/App.js
+++ src/App.js
@@ -2,30 +2,44 @@ import React, { useState } from 'react'
 import { Die } from './components/Die'
 import './App.css'
 
+// 1) Roll the Dice
+// 2) Generate a random number(1-6) for each Die
+// 3) Pass that number into the die
+// 4) Finish!
+// 5) Bonus, keep a tally of all the rolls! using hooks!?  how?
+
 const App = () => {
-  const [number, setNumber] = useState(0)
+  const [dieOne, setDieOne] = useState('one')
+  const [dieTwo, setDieTwo] = useState('six')
   let [rolling, setRolling] = useState(false)
 
   const rollDice = () => {
+    // set the state of 'rolling' to true
     setRolling((rolling = true))
-
+    // instansiate die values in word form for class changes
+    let dieValues = ['one', 'two', 'three', 'four', 'five', 'six']
+    // change die one value
+    setDieOne(dieValues[(Math.random() * dieValues.length) | 0])
+    // change die two value
+    setDieTwo(dieValues[(Math.random() * dieValues.length) | 0])
+    // set the rolling state back to 'false'
     setTimeout(rolling => {
       setRolling((rolling = false))
     }, 1000)
+    return
   }
 
   return (
     <div className="App-header">
       <div className={rolling ? 'dice shakeDice' : 'dice'}>
-        <Die />
-        <Die />
+        <Die value={dieOne} />
+        <Die value={dieTwo} />
       </div>
       <button
         disabled={rolling}
         style={{ fontSize: 20, width: 200, height: 70, marginTop: 50 }}
         onClick={() => {
           rollDice()
-          setNumber(number + 1)
         }}
       >
         <p>{rolling ? 'Rolling...' : 'Roll Dice'}</p>
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
-export const Die = () => {
-  let dieValues = ['one', 'two', 'three', 'four', 'five', 'six']
-  let dieValue = dieValues[(Math.random() * dieValues.length) | 0]
-  let classes = `fas fa-dice-${dieValue} fa-5x`
+export const Die = ({ value }) => {
+  let classes = `fas fa-dice-${value} fa-5x`
   return (
     <div>
       <i
